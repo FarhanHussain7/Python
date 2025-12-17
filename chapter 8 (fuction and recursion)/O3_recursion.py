9aa29bd8d0eed5a0c6c8dc8f940c5ae73670f1b4
Farhanhussain7 <farhanhussain81303@gmail.com>
2025-12-17 23:27:46 +0530

    new changes

--- chapter 8 (fuction and recursion)/O3_recursion.py
+++ chapter 8 (fuction and recursion)/O3_recursion.py
@@ -12,14 +12,16 @@ def factorial_iter(n):
     for i in range(n):
       product = product * (i+1)
     return product
+
+
 def factorial_recursive(n):
    if n == 1 or n ==0:
       return 1
    return n * factorial_recursive(n-1)
 
 
-#f = factorial_iter(5)
-f = factorial_recursive(5)
+#f = factorial_iter(5) # In this calling funtion run only for one single time and run for loop inside 
+f = factorial_recursive(5) # In this calling function keep calling itself again and again if give parameter is greater than 1
 print(f)
 
 
